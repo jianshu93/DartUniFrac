5c4853dbbb953ab39bcf9f56fccb3895d08214e1
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2025-08-26 11:43:19 -0700

    update info

--- src/dartunifrac.rs
+++ src/dartunifrac.rs
@@ -444,7 +444,7 @@ fn main() -> Result<()> {
 
     let m = Command::new("dartunifrac")
         .version("0.2.1")
-        .about(format!("Approximate unweighted UniFrac via Weighted MinHash {dart}{dart}{dart}"))
+        .about(format!("DartUniFrac: Approximate unweighted UniFrac via Weighted MinHash {dart}{dart}{dart}"))
         .arg(
             Arg::new("tree")
                 .short('t')
