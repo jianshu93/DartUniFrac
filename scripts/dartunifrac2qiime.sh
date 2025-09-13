aeda2fe17ac235197018af46fd5df56da15b1575
jianshu93 <jansuechao@gmail.com>
2025-09-13 10:12:04 -0700

    doc and qiime viewer

---
+++ scripts/dartunifrac2qiime.sh
@@ -0,0 +1,5 @@
+qiime tools import --input-path ./GWMC_unifrac_dmh.tsv --type 'DistanceMatrix' --output-path ./GWMC_unifrac_dmh.qza
+qiime diversity pcoa --i-distance-matrix ./GWMC_unifrac_dmh.qza --o-pcoa ./GWMC_unifrac_dmh_pcoa.qza
+qiime emperor plot --i-pcoa ./GWMC_unifrac_dmh_pcoa.qza --m-metadata-file ./GWMC_metadata_origin.tsv --o-visualization GWMC_unifrac_dmh_emperor.qzv
+
+### visualize it at https://view.qiime2.org by uploading GWMC_unifrac_dmh_emperor.qzv
