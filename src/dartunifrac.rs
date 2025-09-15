13851a89631573e311529056e7136b7419340398
jianshu93 <jansuechao@gmail.com>
2025-09-14 17:32:14 -0700

    v0.2.4

--- src/dartunifrac.rs
+++ src/dartunifrac.rs
@@ -1209,7 +1209,7 @@ fn main() -> Result<()> {
         .unwrap_or("🎯");
 
     let m = Command::new("dartunifrac")
-        .version("0.2.3")
+        .version("0.2.4")
         .about(format!("DartUniFrac: Approximate UniFrac via Weighted MinHash {dart}{dart}{dart}"))
         .arg(
             Arg::new("tree")
